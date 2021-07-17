fceb5e94ae0d8bb1114f9b482ef7c81ee81b69c4
KartikCD <chawdakartik@gmail.com>
2021-07-17 05:51:03 +0530

    navigation setup for auth and feed.
    Author: Kartik Chawda

--- app/src/main/java/com/kartikcd/mediumx/MainActivity.kt
+++ app/src/main/java/com/kartikcd/mediumx/MainActivity.kt
@@ -2,10 +2,51 @@ package com.kartikcd.mediumx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.kartikcd.mediumx.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding?.root)
+
+        navController = findNavController(R.id.fragment_navigation)
+
+        setSupportActionBar(_binding?.mainToolbar)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_auth, menu);
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_feed -> {
+                println("Debug: main feed")
+                navController.navigate(R.id.gotoFeedFragment)
+            }
+            R.id.menu_login_signup -> {
+                println("Debug: login feed")
+                navController.navigate(R.id.gotoAuthFragment)
+            }
+            R.id.menu_about -> {
+                println("Debug: about feed")
+            }
+        }
+        return true
     }
 }
--- app/src/main/java/com/kartikcd/mediumx/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/kartikcd/mediumx/ui/auth/AuthFragment.kt
@@ -6,27 +6,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.android.material.tabs.TabLayout
 import com.kartikcd.mediumx.R
+import com.kartikcd.mediumx.databinding.FragmentAuthBinding
 
 class AuthFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AuthFragment()
-    }
-
-    private lateinit var viewModel: AuthViewModel
+    private var _binding: FragmentAuthBinding? = null
+    private var navController: NavController? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+
+        return _binding?.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
-        // TODO: Use the ViewModel
+        navController = _binding?.let {
+            Navigation.findNavController(it.root.findViewById(R.id.authFragmentNavHost))
+        }
+
+        _binding?.authTabLayout?.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position) {
+                    0 -> {
+                        navController?.navigate(R.id.gotoLoginFragment)
+                    }
+                    1 -> {
+                        navController?.navigate(R.id.gotoSignUpFragment)
+                    }
+                }
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
